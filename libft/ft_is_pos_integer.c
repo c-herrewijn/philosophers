646126e09e21af10197225ea770fe877df1ef961
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-06-29 18:30:22 +0200

    bugfix in libft

--- libft/ft_is_pos_integer.c
+++ libft/ft_is_pos_integer.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_is_positive_integer.c                           :+:    :+:            */
+/*   ft_is_pos_integer.c                                :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/21 13:34:47 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/06/29 17:44:59 by cherrewi      ########   odam.nl         */
+/*   Updated: 2023/06/29 18:24:13 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ optionally starts with a plus '+' or minus '-' sign
 only consists of digits [0-9]
 is within range [0, INT_MAX]
 */
-bool	ft_is_positive_integer(char *int_str)
+bool	ft_is_pos_integer(char *int_str)
 {
 	return (ft_isinteger(int_str) && ft_atoi(int_str) >= 0);
 }
