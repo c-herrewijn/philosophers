f0774bda37b18c9b6af747dff8b14a325a35ac7a
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-07-07 16:58:42 +0200

    moved data init out of main

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/26 11:55:02 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/07/07 16:49:28 by cherrewi      ########   odam.nl         */
+/*   Updated: 2023/07/07 16:57:14 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,21 @@ void	free_all(t_data *data)
 }
 
 // put all pointer variables to NULL, for better error handling
-static void	init_data(t_data *data)
+static int	init_data(t_data *data, t_settings *settings, pthread_mutex_t *settings_lock)
 {
 	data->forks = NULL;
 	data->philosophers = NULL;
 	data->threads = NULL;
+	data->forks = create_cutlery(data, settings);
+	if (data->forks == NULL)
+		return (-1);
+	data->philosophers = create_philosophers(data, settings, settings_lock);
+	if (data->philosophers == NULL)
+		return (-1);
+	data->threads = create_thread_arr(data, settings);
+	if (data->threads == NULL)
+		return (-1);
+	return (0);
 }
 
 static int	join_threads(pthread_t **threads)
@@ -47,23 +57,15 @@ static int	join_threads(pthread_t **threads)
 int	main(int argc, char *argv[])
 {
 	t_settings		settings;
-	pthread_mutex_t	settings_lock;
 	t_data			data;
+	pthread_mutex_t	settings_lock;
 
 	if (input_valid(argc, argv) == false)
 		return (1);
 	store_inputs(argc, argv, &settings);
-	init_data(&data);
-	data.forks = create_cutlery(&data, &settings);
-	if (data.forks == NULL)
+	if (init_data(&data, &settings, &settings_lock) < 0)
 		return (1);
-	data.philosophers = create_philosophers(&data, &settings, &settings_lock);
-	if (data.philosophers == NULL)
-		return (1);
-	data.threads = create_thread_arr(&data, &settings);
-	if (data.threads == NULL)
-		return (1);
-	
+
 	pthread_mutex_init(&settings_lock, NULL);
 	if (launch_threads(&settings, data.philosophers, data.threads) < 0)
 		return (1);
