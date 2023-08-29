f0b1254a943a7ee11ee2db41c8a9a88a51063170
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-08-29 18:47:46 +0200

    fixed bug in priorities

--- philo_bonus/priority.c
+++ philo_bonus/priority.c
@@ -6,48 +6,55 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/29 14:30:27 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/08/29 16:30:39 by cherrewi      ########   odam.nl         */
+/*   Updated: 2023/08/29 18:45:50 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-/*
-In case of odd nr of philos:
-	First half GIVE priority after even nr of eatings (except zero)
-	Second half GIVE priority after odd nr of eatings
-	Last filo GIVE priority after even nr of eatings
-*/
-static void	priority_first_round(t_philosopher *philo, t_settings *settings,
+static void	priority_even_philos(t_philosopher *philo, t_settings *settings,
 	struct timeval *now)
 {
-	if (settings->nr_philo % 2 == 1 && philo->nr == settings->nr_philo)
-		ms_sleep(1 + settings->time_to_eat + settings->time_to_sleep / 2, now);
-	else
+	if (calc_ms_passed(&(settings->start_time), now) < settings->time_to_eat)
 	{
-		if (philo->nr % 2 == 1)
-			ms_sleep(1 + settings->time_to_eat / 2, now);
+		if (philo->nr % 2 == 0)
+			ms_sleep(settings->time_to_eat, now);
 	}
 }
 
-// Odd philosophers GIVE priority in the first round
-static void	priority_odd_philos(t_philosopher *philo, t_settings *settings,
+/*
+Odd philosophers GIVE priority in the first round
+First half GIVE priority after even nr of eatings (except zero)
+Second half GIVE priority after odd nr of eatings
+Last filo GIVE priority after even nr of eatings
+*/
+static void	priority_odd_philos(t_philosopher *philo, t_settings *set,
 	struct timeval *now)
 {
-	if (philo->nr == settings->nr_philo)
+	if (calc_ms_passed(&(set->start_time), now) < set->time_to_eat)
 	{
-		if (philo->times_eaten % 2 == 0)
-			ms_sleep(2 + settings->time_to_eat / 2, now);
-	}
-	else if (philo->nr <= settings->nr_philo / 2)
-	{
-		if (philo->times_eaten > 0 && (philo->times_eaten % 2) == 0)
-			ms_sleep(2 + settings->time_to_eat / 2, now);
+		if (philo->nr == set->nr_philo)
+			ms_sleep(1 + set->time_to_eat + set->time_to_sleep / 2, now);
+		else if (philo->nr % 2 == 1)
+			ms_sleep(1 + set->time_to_eat / 2, now);
 	}
 	else
 	{
-		if (philo->times_eaten % 2 == 1)
-			ms_sleep(2 + settings->time_to_eat / 2, now);
+		if (philo->nr == set->nr_philo)
+		{
+			if (philo->times_eaten % 2 == 0)
+				ms_sleep(2 + set->time_to_eat / 2, now);
+		}
+		else if (philo->nr <= set->nr_philo / 2)
+		{
+			if (philo->times_eaten > 0 && (philo->times_eaten % 2) == 0)
+				ms_sleep(2 + set->time_to_eat / 2, now);
+		}
+		else
+		{
+			if (philo->times_eaten % 2 == 1)
+				ms_sleep(2 + set->time_to_eat / 2, now);
+		}
 	}
 }
 
@@ -56,8 +63,8 @@ void	give_priority(t_philosopher *philo, t_settings *settings)
 	struct timeval	now;
 
 	gettimeofday(&now, NULL);
-	if (calc_ms_passed(&(settings->start_time), &now) < settings->time_to_eat)
-		priority_first_round(philo, settings, &now);
-	else if (settings->nr_philo % 2 == 1)
+	if (settings->nr_philo % 2 == 1)
 		priority_odd_philos(philo, settings, &now);
+	else
+		priority_even_philos(philo, settings, &now);
 }
